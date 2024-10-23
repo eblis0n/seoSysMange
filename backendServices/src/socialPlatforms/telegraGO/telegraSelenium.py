85e57dc30c1a18c07cbdda7f9b9bf7d87cb02254
eblis0n <eblis0n@hotmail.com>
2024-10-23 15:43:41 +0800

    更改组html方式

--- backendServices/src/socialPlatforms/telegraGO/telegraSelenium.py
+++ backendServices/src/socialPlatforms/telegraGO/telegraSelenium.py
@@ -178,21 +178,26 @@ class telegraSelenium():
                 return None
             else:
                 print("this_links:", len(this_links), this_links)
-
+                all_atab = ''
                 # 插入链接
                 for link in this_links:
-                    link = link.strip('\n\n')
-                    driver.execute_script("""
-                            var a = document.createElement('a');
-                            a.href = arguments[0];  // 链接地址
-                            a.textContent = arguments[1];  // 链接文字
-                            a.target = '_blank';  // 打开新标签页
-                            arguments[2].appendChild(a);
+                    link = link.strip('\n')
+                    a_tab = f"""<a href={link} target="_blank">{alt_tex}</a>&nbsp;"""
+                    all_atab += a_tab  # 正确的拼接字符串方式
+                p_tab = f"""<p dir="auto" data-placeholder="Your story...">{all_atab}"""
+                driver.execute_script("arguments[0].innerHTML = arguments[1];", content_input, p_tab)
 
-                            // 添加空格
-                            var space = document.createTextNode('\u00A0');
-                            arguments[2].appendChild(space);
-                        """, link, alt_tex, content_input)
+                # driver.execute_script("""
+                    #         var a = document.createElement('a');
+                    #         a.href = arguments[0];  // 链接地址
+                    #         a.textContent = arguments[1];  // 链接文字
+                    #         a.target = '_blank';  // 打开新标签页
+                    #         arguments[2].appendChild(a);
+                    #
+                    #         // 添加空格
+                    #         var space = document.createTextNode('\u00A0');
+                    #         arguments[2].appendChild(space);
+                    #     """, link, alt_tex, content_input)
 
                 time.sleep(3)
 
